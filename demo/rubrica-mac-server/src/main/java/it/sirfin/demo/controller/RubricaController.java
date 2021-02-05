385768242c0f016bede5546dbf2e0e0ba4c410f8
ValeriaCom <78487304+ValeriaCom@users.noreply.github.com>
2021-02-05 14:25:23 +0100

    Create RubricaController.java

---
+++ demo/rubrica-mac-server/src/main/java/it/sirfin/demo/controller/RubricaController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.sirfin.demo.controller;
+
+/**
+ *
+ * @author 39392
+ */
+public class RubricaController {
+    
+}
