ac28aa9816f18d75970bbc475a9c3002b13d4cb5
marcobonaccorso <marco.bonaccorso00@gmail.com>
2021-02-05 14:11:29 +0100

    codice iniziale del server

---
+++ demo/rubrica-mac-server/src/main/java/it/sirfin/demo/RubricaApplication.java
@@ -0,0 +1,13 @@
+package it.sirfin.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubricaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RubricaApplication.class, args);
+	}
+
+}
