37e99ce6bd6ee9c20b6f8952db825b622c641538
ValeriaCom <78487304+ValeriaCom@users.noreply.github.com>
2021-02-14 16:56:15 +0100

    application properties

--- demo/rubrica-mac-server/src/main/java/it/sirfin/demo/RubricaApplication.java
+++ demo/rubrica-mac-server/src/main/java/it/sirfin/demo/RubricaApplication.java
@@ -2,8 +2,15 @@ package it.sirfin.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"it.sirfin.rubrica"})
+@EnableJpaRepositories(basePackages = {"it.sirfin.rubrica.repository"})
+@EntityScan(basePackages = {"it.sirfin.rubrica.model"})
+
 public class RubricaApplication {
 
 	public static void main(String[] args) {
